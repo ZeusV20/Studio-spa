f7c6f2c59cec7495e680564908c9f8a9611a8563
ZeusV20 <zeusvelazquez48@gmail.com>
2024-02-12 23:56:38 -0600

    design config

--- js/assets.js
+++ js/assets.js
@@ -1,14 +1,9 @@
-function initMap() {
-    var ubicacionNegocio = {lat: 18.252452881530562, lng: -93.22473396845227}; // Reemplaza estas coordenadas con la ubicación real de tu negocio
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15, // Ajusta el zoom según tus necesidades
-      center: ubicacionNegocio
-    });
-    var marker = new google.maps.Marker({
-      position: ubicacionNegocio,
-      map: map,
-      title: 'Tu Negocio' // Añade el nombre de tu negocio aquí
-    });
-  }
+function openModal(element) {
+  var imgSrc = element.getElementsByTagName("img")[0].src;
+  document.getElementById("modalImage").src = imgSrc;
+  document.getElementById("imageModal").style.display = "block";
+}
 
-  
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
